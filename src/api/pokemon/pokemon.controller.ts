28063cca20271b360eeae2d463ac694054dae61f
Endurance Idehen <endurance.idehen@rigup.co>
2020-07-10 14:09:46 -0500

    change import, slight rename

--- src/api/pokemon/pokemon.controller.ts
+++ src/api/pokemon/pokemon.controller.ts
@@ -1,18 +1,17 @@
-import { Controller, Get } from "@nestjs/common";
-import { ApiProperty, ApiResponse, ApiTags } from "@nestjs/swagger";
+import { Controller, Get, Param } from "@nestjs/common";
+import { ApiProperty, ApiTags } from "@nestjs/swagger";
+import * as Pokedex from "pokedex-promise-v2";
 
-const Pokedex = require("pokedex-promise-v2");
-
-export class Pokemon {
+export class PokemonItem {
   @ApiProperty()
   name: string;
   @ApiProperty()
-  url: string;
+  url?: string | any;
 }
 
 export class PokemonResponse {
-  @ApiProperty({ type: [Pokemon] })
-  pokemon: Pokemon[];
+  @ApiProperty({ type: [PokemonItem] })
+  pokemon: PokemonItem[];
 }
 
 @ApiTags("pokemon")
@@ -21,14 +20,16 @@ export class PokemonController {
   private _pokedex = new Pokedex();
   
   @Get("/")
-  @ApiResponse({
-    description: "Response received",
-    type: PokemonResponse
-  })
-  public async allPokemon(): Promise<PokemonResponse> {
+  public async getAllPokemon(): Promise<PokemonResponse> {
     const answer = await this._pokedex.getPokemonsList();
     return {
       pokemon: answer.results
     };
+  }
+  
+  @Get("/:id")
+  public async getPokemonById(@Param('id') id: string): Promise<PokemonItem> {
+    const answer = await this._pokedex.getPokemonByName(id);
+    return answer as unknown as PokemonItem;
   }
 }
